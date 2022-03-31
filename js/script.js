13a57ce1bb1d38260f0b2f3298aa24d5a60c76e0
wiklapandu <wiklapandu2503@gmail.com>
2022-03-31 14:19:17 +0700

    membuat tampilan baru saat menang, kalah, seri, atau waktu habis

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ let random = 0;
 let sudahJwb = false;
 const start = document.querySelector('#start');
 const me = document.querySelectorAll('button.myHand');
+const ulangBtn = document.querySelectorAll('button.ulang');
+
+// pages
+const menangPages = document.querySelector('#menang');
+const kalahPages = document.querySelector('#kalah');
+const waktuPages = document.querySelector('#waktu-habis');
+const seriPages = document.querySelector('#seri');
+
 function rules(me, enemy) {
     if (me === enemy) {
+
+        document.querySelector('#content').style.display = 'none'
+        document.querySelector('#seri').style.display = 'flex'
         swal({
             title: 'seri 🤜🏼🤛🏼',
             icon: 'warning',
@@ -15,6 +26,8 @@ function rules(me, enemy) {
         })
     }
     else if ((me === '👊🏼' && enemy === '✋🏼') || (me === '✋🏼' && enemy === '✌🏼') || (me === '✌🏼' && enemy === '👊🏼')) {
+        document.querySelector('#content').style.display = 'none'
+        document.querySelector('#kalah').style.display = 'flex'
         swal({
             title: 'Kalah ❌❌❌',
             icon: 'error',
@@ -22,6 +35,8 @@ function rules(me, enemy) {
             className: 'alert',
         });
     } else {
+        document.querySelector('#content').style.display = 'none'
+        document.querySelector('#menang').style.display = 'flex'
         swal({
             title: 'Menang ✔✔✔',
             icon: 'success',
@@ -42,7 +57,9 @@ me.forEach(hand => {
     });
 });
 
-start.addEventListener('click', () => {
+// ? Setup Function
+
+function setupGame() {
     timer = 3;
     random = Math.floor(Math.random() * 3);
     document.querySelector('span#enemy').innerHTML = '?';
@@ -55,6 +72,8 @@ start.addEventListener('click', () => {
             clearInterval(intervalTimer);
             if (!sudahJwb) {
                 // swal()
+                document.querySelector('#content').style.display = 'none'
+                document.querySelector('#waktu-habis').style.display = 'flex'
                 swal({
                     title: 'Waktu Habis ❗',
                     icon: 'info',
@@ -64,4 +83,18 @@ start.addEventListener('click', () => {
             }
         }
     }, 1000)
+}
+
+start.addEventListener('click', () => {
+    setupGame();
 });
+
+ulangBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector(`#${btn.parentElement.id}`).style.display = 'none';
+        document.querySelector('#content').style.display = 'flex'
+        setTimeout(() => {
+            setupGame();
+        }, 1000)
+    })
+})
